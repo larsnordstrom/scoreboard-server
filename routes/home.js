74a3a9a762c1aa843abb085db67e46b4413fda8a
larsnordstrom <lars.nordstrom@jgy.se>
2020-02-06 13:34:50 +0100

    Created home route.

---
+++ routes/home.js
@@ -0,0 +1,14 @@
+
+
+
+const express = require("express");
+const router = express.Router();
+
+
+router.get("/", (req, res) => {
+    res.send("<h1>Hello World</h>");
+});
+
+
+
+module.exports = router;
